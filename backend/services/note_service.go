48f65630c7000c51e7ed99a7e33a98885ce11dbb
Zaenul Hilmi <zaenulhilmi@gmail.com>
2022-04-24 23:35:14 +0700

    adding note service

--- backend/services/note_service.go
+++ backend/services/note_service.go
@@ -9,3 +9,21 @@ type NoteService interface {
 	CreateNote(title, content string) (*entities.Note, error)
 	GetNote(id uuid.UUID) (*entities.Note, error)
 }
+
+func NewNoteService() NoteService {
+	return &noteService{}
+}
+
+type noteService struct {
+}
+
+// CreateNote implements NoteService
+func (*noteService) CreateNote(title string, content string) (*entities.Note, error) {
+	panic("unimplemented")
+}
+
+// GetNote implements NoteService
+func (*noteService) GetNote(id uuid.UUID) (*entities.Note, error) {
+    var note *entities.Note
+    return note,nil
+}
